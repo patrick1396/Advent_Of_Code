a0851120ebd0f25e7ece23b597bbc9d42934fd23
Patrick <patrick.williams.1396@gmail.com>
2021-12-11 23:45:21 +0000

    Day 10 done

--- Day_10/day_10.cpp
+++ Day_10/day_10.cpp
@@ -5,18 +5,23 @@ using namespace std;
 
 int main()
 {
-  string line, sub, add, end;
+  string line, sub, add, end, comp, chr;
   int file_length = 90, len, sub_len;
   int i, j;
-  int err_tot = 0;
+  int num_incmplt = 0;
+  int err_score = 0;
+  long long int auto_score, hold;
+  long long int auto_array[file_length];
   bool corrupt;
   
   ifstream myfile;
   myfile.open("input.txt");
+
   
   
   for (i=0; i<file_length; i++)
     {
+      auto_array[i] = 0;
       getline(myfile, line);
       len = line.length();
       sub = line[0];
@@ -45,24 +50,96 @@ int main()
 	}
       if (corrupt==true)
 	{
+
 	  if (add == ")")
 	    {
-	      err_tot += 3;
+	      err_score += 3;
 	    }
 	  else if (add == "]")
 	    {
-	      err_tot += 57;
+	      err_score += 57;
 	    }
 	  else if (add == "}")
 	    {
-	      err_tot += 1197;
+	      err_score += 1197;
 	    }
 	  else if (add == ">")
 	    {
-	      err_tot += 25137;
+	      err_score += 25137;
+	    }
+	}
+      else
+	{
+	  num_incmplt ++;
+	  sub_len = sub.length();
+	  comp = "";
+	  for (j=sub_len-1; j>=0; j--)
+	    {
+	      chr = sub[j];
+	      if (chr == "(")
+		{
+		  comp += ")";
+		}
+	      else if (chr == "[")
+		{
+		  comp += "]";
+		}
+	      else if (chr == "{")
+		{
+		  comp += "}";
+		}
+	      else if (chr == "<")
+		{
+		  comp += ">";
+		}
+	    }
+
+	  for (j=0; j<sub_len; j++)
+	    {
+	      auto_array[i] *= 5;
+	      
+	      chr = comp[j];
+	      
+	      if (chr == ")")
+		{
+		  auto_array[i] += 1;
+		}
+	      else if (chr == "]")
+		{
+		  auto_array[i] += 2;
+		}
+	      else if (chr == "}")
+		{
+		  auto_array[i] += 3;
+		}
+	      else if (chr == ">")
+		{
+		  auto_array[i] += 4;
+		}
 	    }
 	}
     }
   myfile.close();
-  printf("Part 1: Total Syntax Error = %d\n", err_tot);
+
+
+  for (i=0; i<file_length-1; i++)
+    {
+      for (j = 0; j<file_length-1-i; j++)
+	{
+	  if (auto_array[j]>auto_array[j+1])
+	    {
+	      hold = auto_array[j];
+	      auto_array[j] = auto_array[j+1];
+	      auto_array[j+1] = hold;
+	    }
+	}
+    }
+
+  
+  auto_score = auto_array[file_length-(num_incmplt+1)/2];
+  
+  printf("Part 1: Total Syntax Error = %d\n", err_score);
+  printf("Part 2: Autocomplete Score = %lld\n", auto_score);
 }
+
+
